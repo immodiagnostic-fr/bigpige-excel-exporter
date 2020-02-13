cdadd0e3d570b62720c034be5becd70d3db3ebbd
AaronFresh <aaronfresh08@gmail.com>
2020-02-13 14:01:13 +0200

    export essai

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -10,7 +10,10 @@ import { Annonce } from '../Annonce';
 export class TableComponent implements OnInit {
 
   data:Annonce[];
+  selectedAnnonces:Annonce[];
   cols:any[];
+  columns: any[];
+  exportColumns: any[];
 
    constructor(private dataService:DataService){}
 
@@ -25,9 +28,50 @@ export class TableComponent implements OnInit {
       { field: 'departement', header: 'Departement' },
       { field: 'codepostal', header: 'Code Postal' }
   ];
+  this.exportColumns = this.cols.map(col => ({title: col.header, dataKey: col.field}));
 
   }
+  exportPdf() {
+    import("jspdf").then(jsPDF => {
+        import("jspdf-autotable").then(x => {
+            const doc = new jsPDF.default(0,0);
+            doc.autoTable(this.columns, this.data);
+            doc.save('primengTable.pdf');
+        })
+    })
+}
 
+
+// exportExcel() {
+//     import("xlsx").then(xlsx => {
+//         const worksheet = xlsx.utils.json_to_sheet(this.getData());
+//         const workbook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
+//         const excelBuffer: any = xlsx.write(workbook, { bookType: 'xlsx', type: 'array' });
+//         this.saveAsExcelFile(excelBuffer, "primengTable");
+//     });
+// }
+
+// saveAsExcelFile(buffer: any, fileName: string): void {
+//   import("file-saver").then(FileSaver => {
+//       let EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+//       let EXCEL_EXTENSION = '.xlsx';
+//       const data: Blob = new Blob([buffer], {
+//           type: EXCEL_TYPE
+//       });
+//       FileSaver.saveAs(data, fileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION);
+//   });
+// }
+
+
+
+getData() {
+    let annonces = [];
+    for(let annonce of this.data) {
+      annonce.id = annonce.id.toString();
+        annonces.push(annonce);
+    }
+    return annonces;
+}
   
 
 }
