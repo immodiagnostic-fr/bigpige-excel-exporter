c05a922ba543448d2ab96fc64a47c31d5ac98e0d
AaronFresh <aaronfresh08@gmail.com>
2020-02-13 14:33:43 +0200

    table export avec les 3 bouttons

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -41,32 +41,33 @@ export class TableComponent implements OnInit {
 }
 
 
-// exportExcel() {
-//     import("xlsx").then(xlsx => {
-//         const worksheet = xlsx.utils.json_to_sheet(this.getData());
-//         const workbook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
-//         const excelBuffer: any = xlsx.write(workbook, { bookType: 'xlsx', type: 'array' });
-//         this.saveAsExcelFile(excelBuffer, "primengTable");
-//     });
-// }
+exportExcel() {
+    import("xlsx").then(xlsx => {
+        const worksheet = xlsx.utils.json_to_sheet(this.getData());
+        const workbook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
+        const excelBuffer: any = xlsx.write(workbook, { bookType: 'xlsx', type: 'array' });
+        this.saveAsExcelFile(excelBuffer, "primengTable");
+    });
+}
 
-// saveAsExcelFile(buffer: any, fileName: string): void {
-//   import("file-saver").then(FileSaver => {
-//       let EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
-//       let EXCEL_EXTENSION = '.xlsx';
-//       const data: Blob = new Blob([buffer], {
-//           type: EXCEL_TYPE
-//       });
-//       FileSaver.saveAs(data, fileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION);
-//   });
-// }
+saveAsExcelFile(buffer: any, fileName: string): void {
+  import("file-saver").then(FileSaver => {
+      let EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+      let EXCEL_EXTENSION = '.xlsx';
+      const data: Blob = new Blob([buffer], {
+          type: EXCEL_TYPE
+      });
+      FileSaver.saveAs(data, fileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION);
+  });
+}
 
 
 
 getData() {
+    //j'ai pas compris cette fonction j'ai mis code postal au hasard
     let annonces = [];
     for(let annonce of this.data) {
-      annonce.id = annonce.id.toString();
+      annonce.codepostal = annonce.codepostal.toString();
         annonces.push(annonce);
     }
     return annonces;
