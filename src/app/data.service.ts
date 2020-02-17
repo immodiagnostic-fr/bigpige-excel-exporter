1384602f324ae2fd25fcf171860428756a6c3020
Avner Hadjadj <hadj.avner@gmail.com>
2020-02-17 17:25:13 +0200

    ajout requetes getRegions et getRegions pour remplir les autocomplete

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,16 +1,23 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment as env } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor(private http: HttpClient) {
-    
-  }
+  constructor(private http: HttpClient) {}
 
   getAnnonces() {
     return this.http.get('assets/annonces.json');
+  }
+
+  getRegions() {
+  	return this.http.get(env.api_host + "/regions");
+  }
+
+  getCategories() {
+  	return this.http.get(env.api_host + "/annonces/categories");
   }
 }
