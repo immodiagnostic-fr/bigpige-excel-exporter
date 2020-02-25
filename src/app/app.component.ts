7dcf4f80d8e800783b808738262b4b0f8b421710
Avner Hadjadj <hadj.avner@gmail.com>
2020-02-26 00:45:59 +0200

    fix noms des champs a envoyer et champs ville et code postal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Validators, FormGroup, FormBuilder } from '@angular/forms';
 import { DataService } from './data.service';
 import { forkJoin } from 'rxjs';
 import { Annonce } from './Annonce';
-import { SelectItem } from 'primeng/api/selectitem';
+import { SelectItem } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
@@ -61,7 +61,7 @@ export class AppComponent implements OnInit {
       //this.categories = res[2].map(c => c.categorie);     
       this.regions = [];
           for (let i = 0; i < res[1].length; i++) {
-              this.regions.push({label: res[1][i].region_label, value:  res[1][i].region_label});
+              this.regions.push({label: res[1][i].nom, value:  res[1][i].region_label});
           }
       this.categories = [];
           for (let i = 0; i < res[2].length; i++) {
@@ -117,9 +117,11 @@ export class AppComponent implements OnInit {
       "date_maximum":  ob.stopDate ? ob.stopDate : null,
       "prix_minimum":  ob.rangeValues ? ob.rangeValues[0] : null,
       "prix_maximum":  ob.rangeValues ? ob.rangeValues[1] : null,
-      "selectedRegions":  ob.selectedRegions ? ob.selectedRegions : null,
-      "selectedCategories":  ob.selectedCategories ? ob.selectedCategories : null,
-      "selectedDepartements": ob.selectedDepartements ? ob.selectedDepartements : null,
+      "region":  ob.selectedRegions ? ob.selectedRegions : null,
+      "ville":  ob.ville ? ob.ville : null,
+      "code_postal":  ob.codePostal ? ob.codePostal : null,
+      "categorie":  ob.selectedCategories ? ob.selectedCategories : null,
+      "departement_code": ob.selectedDepartements ? ob.selectedDepartements : null,
       "telephone":  ob.telephone ? ob.telephone : null
     }
 
